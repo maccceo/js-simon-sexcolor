c8983379827174c8343d894226fe334b8f158e97
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-15 18:36:25 +0200

    simon - rettifica push precedente

--- simon/main.js
+++ simon/main.js
@@ -1,3 +1,30 @@
 // Un alert espone 5 numeri casuali. Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l’utente deve inserire un prompt alla volta i numeri che ha visto precedentemente.
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati;
+var numeri = [];
+
+// genero i 5 numeri casuali
+for (var i = 0; i < 5; i++) {
+	numeri.push(Math.floor(Math.random() * 50 + 1));
+}
+console.log('Numeri da indovinare: ' + numeri);
+
+// li visualizzo
+alert("Ricordati questi numeri: " + numeri);
+
+//parte il timer e indico la funzione da fare a tempo scaduto
+setTimeout(contaRisposteGiuste,3000);
+
+
+function contaRisposteGiuste() {
+	console.log("Tempo scaduto!");
+	var input, numeriInseriti = [], numeriEsatti = [];
+
+	for (var i = 0; i < 5; i++) {
+		input = prompt("Inserisci un numero che ti ricordi:");
+		// controllo che sia presente nei numeri da ricordare
+		numeriInseriti.push(input);
+	}
+	console.log('Numeri ricordati: ' + numeriInseriti);
+}
+
