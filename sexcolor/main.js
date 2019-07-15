5ac667e6d3930b7f845ba57e9e5480fbdd664676
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-15 19:11:00 +0200

    sexcolor - controllo input M o F

--- sexcolor/main.js
+++ sexcolor/main.js
@@ -6,14 +6,23 @@ var h1Element = document.getElementById("name");
 
 var nome = prompt("Ciao, inserisci il tuo nome:");
 var sesso = prompt("Inserisci il sesso (M o F):").toLowerCase();
-console.log(sesso);
+// controllo che l'utente abbia inserito un formato corretto
+var inputCorretto = false;
+while (inputCorretto === false) {
+	if (sesso === "m" || sesso === "f") {
+		inputCorretto = true;
+	} else {
+		sesso = prompt("No dai, inserisci il sesso (M o F):").toLowerCase();
+	}
+}
 
 // converto il sesso dell'utente in colore
 if (sesso === "m") {
-	sesso = "#3078ff";
+	sesso = "#3078ff";	// blu
 } else if  (sesso === "f") {
-	sesso = "#f87afa";
+	sesso = "#f87afa";	// rosa
 }
 
+// stampo nell'HTML il nome col colore del sesso
 h1Element.innerHTML = "Ciao " + nome + "!";
 h1Element.style.color = sesso;
