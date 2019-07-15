d0856b8e4ad783d437f92f2563a68d1e3ee84d34
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-15 19:04:31 +0200

    sexcolor - esercizio completato

--- sexcolor/main.js
+++ sexcolor/main.js
@@ -1,2 +1,19 @@
 // Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
 // Sulla pagina html deve apparire “Ciao <nome>“, il colore del nome deve essere azzurro o rosa a seconda del sesso inserito
+
+// oggetto DOM con l'elemento HTML <h1>
+var h1Element = document.getElementById("name");
+
+var nome = prompt("Ciao, inserisci il tuo nome:");
+var sesso = prompt("Inserisci il sesso (M o F):").toLowerCase();
+console.log(sesso);
+
+// converto il sesso dell'utente in colore
+if (sesso === "m") {
+	sesso = "#3078ff";
+} else if  (sesso === "f") {
+	sesso = "#f87afa";
+}
+
+h1Element.innerHTML = "Ciao " + nome + "!";
+h1Element.style.color = sesso;
